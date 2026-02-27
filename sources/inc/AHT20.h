33cc59784ea069a9866b355b1705b79c760cc44f
O431 <2087858302@qq.com>
2026-02-27 20:59:00 +0800

    AHT20驱动文件

--- sources/inc/AHT20.h
+++ sources/inc/AHT20.h
@@ -9,28 +9,28 @@ extern "C" {
 
     constexpr uint8_t AHT20_ADDR =      0x38;
     constexpr uint8_t AHT_RESET =       0xBA;
+    inline uint8_t AHT_READ =           0x71;
     constexpr uint8_t AHT_INIT[3] =     {0xBE, 0x08, 0x00};
     constexpr uint8_t AHT_TIGGER[3] =   {0xAC, 0X33, 0x00};
+    namespace O431{
+        class AHT20 {
+        public:
+            AHT20();                                                    // 构造函数，默认I2C接口1
 
-    class AHT20 {
-    public:
-        AHT20();                                                    // 构造函数，默认I2C接口1
+            explicit AHT20(I2C_HandleTypeDef *HI2C);                    // 含参构造函数，用于指定I2C接口
+            ~AHT20();
 
-        explicit AHT20(I2C_HandleTypeDef *HI2C);                    // 含参构造函数，用于指定I2C接口
-        ~AHT20();
+            void AHT_Read();                                            // 数值读取函数
+            [[nodiscard]] float get_Value_Temperature() const;          // 向外部提供温度数据接口
+            [[nodiscard]] float get_Value_Humidity() const;             // 向外部提供湿度数据接口
 
-        void AHT_Read();                                            // 数值读取函数
-        [[nodiscard]] float get_Value_Temperature() const;          // 向外部提供温度数据接口
-        [[nodiscard]] float get_Value_Humidity() const;             // 向外部提供湿度数据接口
+        private:
+            I2C_HandleTypeDef *hi2c;
+            float temperature;
+            float humidity;
 
-    private:
-        I2C_HandleTypeDef *hi2c;
-        float temperature;
-        float humidity;
-
-    };
-
-
+        };
+    }
 
 #ifdef __cplusplus
 }
--- sources/src/AHT20.cpp
+++ sources/src/AHT20.cpp
@@ -2,34 +2,36 @@
 #include "i2c.h"
 
 uint8_t test_AHT20(I2C_HandleTypeDef* HI2C);
+float cal_Temperature(float date);
+float cal_Humidity(float date);
 
-AHT20::AHT20() {
+O431::AHT20::AHT20() {
     hi2c = &hi2c1;
     temperature = 0;
     humidity = 0;
-    HAL_I2C_Init(&hi2c1);
+    HAL_I2C_Init(hi2c);
     HAL_Delay(100);
     if ((test_AHT20(hi2c) & 0x08) == 0x00){
         HAL_I2C_Master_Transmit(&hi2c1,AHT20_ADDR,const_cast<uint8_t *>(AHT_INIT),3,TIME_OUT_TIME);
     }
 }
 
-AHT20::AHT20(I2C_HandleTypeDef *HI2C) {
+O431::AHT20::AHT20(I2C_HandleTypeDef *HI2C) {
     hi2c = HI2C;
     temperature = 0;
     humidity = 0;
     HAL_I2C_Init(hi2c);
     HAL_Delay(40);
     if (test_AHT20(hi2c)) {
-        HAL_I2C_Master_Transmit(&hi2c1,AHT20_ADDR,const_cast<uint8_t *>(AHT_INIT),3,TIME_OUT_TIME);
+        HAL_I2C_Master_Transmit(hi2c,AHT20_ADDR,const_cast<uint8_t *>(AHT_INIT),3,TIME_OUT_TIME);
     }
 }
 
-AHT20::~AHT20() {
-    HAL_I2C_DeInit(&hi2c1);
+O431::AHT20::~AHT20() {
+    return;
 }
 
-void AHT20::AHT_Read() {
+void O431::AHT20::AHT_Read() {
     uint8_t buffer_Receive[6];
     HAL_I2C_Master_Transmit(hi2c,AHT20_ADDR,const_cast<uint8_t *>(AHT_TIGGER),3,TIME_OUT_TIME);
     HAL_Delay(80);
@@ -39,23 +41,23 @@ void AHT20::AHT_Read() {
                                  (static_cast<uint32_t>(buffer_Receive[2]) << 4) +
                                  (static_cast<uint32_t>(buffer_Receive[3]) >> 4);
 
-        humidity = static_cast<float>(date) * 100.0f / (1 << 20);
+        humidity = cal_Humidity(static_cast<float>(date));
 
 
         date = ((static_cast<uint32_t>(buffer_Receive[3]) & 0x0F) << 16) +
                                     (static_cast<uint32_t>(buffer_Receive[4]) << 8) +
                                     static_cast<uint32_t>(buffer_Receive[5]);
 
-        temperature = static_cast<float>(date) * 200.0f / (1 << 20) - 50.0f;
+        temperature = cal_Temperature(static_cast<float>(date));
     }
 
 }
 
-float AHT20::get_Value_Humidity() const {
+float O431::AHT20::get_Value_Humidity() const {
     return humidity;
 }
 
-float AHT20::get_Value_Temperature() const {
+float O431::AHT20::get_Value_Temperature() const {
     return temperature;
 }
 
@@ -64,4 +66,11 @@ uint8_t test_AHT20(I2C_HandleTypeDef* HI2C) {
     HAL_I2C_Master_Transmit(HI2C,AHT20_ADDR,reinterpret_cast<uint8_t *>(0x71),2,TIME_OUT_TIME);
     HAL_I2C_Master_Receive(HI2C,AHT20_ADDR,&buffer_Receive,2,TIME_OUT_TIME);
     return buffer_Receive;
+}
+
+float cal_Temperature(float date) {
+    return date * 200.0f / (1 << 20) - 50.0f;
+}
+float cal_Humidity(float date) {
+    return date * 100.0f / (1 << 20);
 }
